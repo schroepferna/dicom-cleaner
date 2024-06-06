b4d7fb7f9d1c2375b11cb5a1f244fffacf9ca4e6
Nancy Schroepfer <155658155+schroepferna@users.noreply.github.com>
2024-06-06 12:35:32 -0500

    Upload the entire project

---
+++ src/dicom-fields.py
@@ -0,0 +1,32 @@
+from deid.dicom import get_files
+from deid.dicom import get_identifiers
+import os
+
+# this file gives some information about dicom fields
+
+# fetch a list of dicom files
+current_folder = os.path.basename(os.getcwd())
+root_path = os.getcwd()[:os.getcwd().index(current_folder) - 1]
+unclean_images_dir = os.path.abspath(os.path.join(root_path, "dicom-images/unclean/case1"))
+dicom_files = list(get_files(unclean_images_dir))
+
+# get the identifiers of the dicom files
+# the identifiers contain sensitive information in the headers
+# get_identifiers returns a dictionary
+# each key of the dictionary is the absolute path of a dicom file
+# each value of the dictionary is another dictionary with expanded strings of tags, a tag looks like (0008, 0005)
+ids = get_identifiers(dicom_files)
+
+# for example
+# print(ids[dicom_files[0]])
+# returns a dictionary with keys like (0010, 0010), and the value is the expanded string of (0010, 0010)
+# (0010, 0010) is a tag for PatientName, it's usually present in most dicom images
+# print(ids[dicom_files[0]]['(0010, 0010)'])
+# returns something like (0010, 0010) Patient's Name                      PN: 'nameless waterfall'  [PatientName]
+# note: 'PatientName' is a field name that can be used in a recipe
+
+# check all the field names present in a dicom file, so we can use it in the recipe
+# all other dicom files in the same folder usually contain similar fields, so just look at one of them
+for key in ids[dicom_files[0]]:
+    field = ids[dicom_files[0]][key]
+    print(field.uid, "->", field.name)
